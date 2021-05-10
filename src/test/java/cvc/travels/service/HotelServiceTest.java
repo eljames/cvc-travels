d865ff85c1d25f70923de9c4a66729376b5c9cac
eljames <eljames4@hotmail.com>
2021-05-10 15:30:13 -0300

    Adding checkIN/OUT range date test

--- src/test/java/cvc/travels/service/HotelServiceTest.java
+++ src/test/java/cvc/travels/service/HotelServiceTest.java
@@ -11,7 +11,7 @@ import cvc.travels.business.TreatableException;
 public class HotelServiceTest {
 
 	@Test
-	public void mustWhenCheckinCheckoutRangeIsInvalid() throws Exception {
+	public void mustBeInvalidWhenCheckinAfterCheckoutOnHotelsByCity() throws Exception {
 		InputParameters params = new InputParameters(
 			"2020-05-15",
 			"2020-05-10",
@@ -21,6 +21,24 @@ public class HotelServiceTest {
 		HotelService hotelservice = new HotelService();
 		try {
 			hotelservice.hotelsByCity(1, params);
+		} catch (TreatableException e) {
+			assertEquals(ErrorCodes.CHECKIN_CHECKOUT_RANGE_INVALID, e.getErrorCode());
+			return;
+		}
+		throw new Exception("The exception was not thrown.");
+	}
+	
+	@Test
+	public void mustBeInvalidWhenCheckinAfterCheckoutOnHotelByHotelId() throws Exception {
+		InputParameters params = new InputParameters(
+			"2020-05-13",
+			"2020-05-10",
+			2,
+			3
+		);
+		HotelService hotelservice = new HotelService();
+		try {
+			hotelservice.hotel(1, params);
 		} catch (TreatableException e) {
 			assertEquals(ErrorCodes.CHECKIN_CHECKOUT_RANGE_INVALID, e.getErrorCode());
 			return;
